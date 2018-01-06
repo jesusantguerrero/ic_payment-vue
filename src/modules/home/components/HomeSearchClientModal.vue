d9e4c2a7897a0c212cebae68e4dbdd9cbe73bea6
jesusantguerrero <me@jesusantguerrero.com>
2018-01-06 02:17:44 -0400

    busqueda de clientes en home

--- src/modules/home/components/HomeSearchClientModal.vue
+++ src/modules/home/components/HomeSearchClientModal.vue
@@ -4,37 +4,26 @@
       .modal-content
         .modal-header
           button(type="button", class="close", data-dismiss="modal", aria-label="Close"): span(aria-hidden="true") &times;
-          h4.modal-title Nuevo Contrato
+          h4.modal-title Buscar Cliente
         .modal-body
-            #search-client-container
-              .searcher-container.main-toolbar#search-client-toolbar(style="margin-bottom: 0")
-                .input-group.search(style="width: 100%; margin-bottom: 5px")
-                  .input-group-addon: i.material-icons search
-                  input.form-control.searcher#client-searcher(type="text",v-model="search", @keyup="getClients", :placeholder="placeholder")
-
-              DataTable(ids="search-client-table", :parentId="parentId", :data="clients", :cols="cols", :toolbar="toolbar", :options="tableOptions")
-        .modal-footer
-          button(type="button", class="btn", data-dismiss="modal") Cancelar
-          button(type="button", class="btn save", id="btn-send-message", @click.stop.prevent='sendMessage') enviar
+          SelectClient(the-id="client-id", parent-id="#search-client-modal",:endpoint="searchEndpoint", @select="goToClient")
 
 </template>
 
 <script>
-  import DataTable from './../../sharedComponents/DataTable';
+  import SelectClient from './../../sharedComponents/SelectClient';
   import ClientStore from './../../clientes/store/clientStore';
 
   export default {
     components: {
-      DataTable
+      SelectClient
     },
 
     data() {
       const clientStore = new ClientStore();
 
       return {
-        parentId: '#search-client-container',
-        toolbar: '#search-client-toolbar',
-        clients: '',
+        searchEndpoint: `${baseURL}/clients/get_clients/dropdown`,
         tableOptions: {
           pageSize: 5
         },
@@ -47,16 +36,9 @@
     },
 
     methods: {
-      getClients() {
-        const data = { word: this.search };
-        this.$http.post('clients/get_clients/search', this.getDataForm(data))
-          .then((res) => {
-            this.clients = res.data.clients;
-          })
-          .catch((err) => {
-            this.$toasted.error(err);
-          });
-      }
+      goToClient(data) {
+        window.location.href = `${baseURL}app/details/${data.id}`;
+      },
     }
   };
 </script>
