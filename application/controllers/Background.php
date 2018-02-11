061e97fbab47d7d31a2c03d7d144feca92b62140
jesusantguerrero <me@jesusantguerrero.com>
2018-02-10 20:32:54 -0400

    background controller desde producción

--- application/controllers/Background.php
+++ application/controllers/Background.php
@@ -1,4 +1,4 @@
-<?php
+<?php 
 class Background extends CI_Controller{
   public function __construct() {
     parent::__construct();
@@ -21,16 +21,18 @@ class Background extends CI_Controller{
   }
 
   private function update_moras() {
-    $today = date('Y-m-d');
-    $settings = $this->settings_model->get_settings();
-
+    $today      = date('Y-m-d');
+    $settings   = $this->settings_model->get_settings();
+    $corte      = $settings['fecha_corte'] + 1;
     $next_check = $settings['next_check'];
+
     if ($next_check == $today) {
       $moras = $this->payment_model->get_moras_view("group");
       $this->update_state_moras($moras, $this);
       if ($moras) {
         $data = $this->payment_model->get_moras_view();
-        if(date('d') == $settings['fecha_corte'] + 1){
+        if (date('d') == $corte) {
+          var_dump('yes it time to cut');
           $this->prepare_moras($data, $settings);
           $this->suspension_automatica();
         }
@@ -38,7 +40,7 @@ class Background extends CI_Controller{
       $result = $this->settings_model->update('last_check_moras', $today);
     }
   }
-
+  
   private function prepare_moras($data, $settings) {
     foreach ($data as $pago) {
       $fecha = date($pago['fecha_limite']);
@@ -47,20 +49,20 @@ class Background extends CI_Controller{
       $mora  = ($settings['cargo_mora'] / 100) * $cuota;
       $this->payment_model->set_extra([0 => ["servicio" => "Reconexion", "precio"=> $settings['reconexion']]], $pago['id_pago']);
       $extras = $this->payment_model->get_extras($pago['id_pago'], true);
-
+  
       $total = $pago['cuota'] + $extras['total'] + $mora;
-
+  
       $updated_data = array(
         'mora'    => $mora,
         'total'   => $total,
         'monto_extra' => $extras['total'],
         'detalles_extra' => $extras['detalles']
       );
-
+  
       $result = $this->payment_model->update($updated_data, $pago['id_pago']);
     }
   }
-
+  
   private function update_state_moras($data){
     foreach ($data as $pago) {
       if($pago['estado_cliente'] != 'activo'){
@@ -68,29 +70,28 @@ class Background extends CI_Controller{
       } else {
         $estado = 'mora';
       }
-
       $this->db->where('id_cliente',$pago['id_cliente']);
-      $this->db->update('ic_clientes',array('estado'=> $estado));
+      $this->db->update('ic_clientes', array('estado'=> $estado));
     }
   }
 
   private function suspension_automatica() {
-    $this->db->where('pagos_generados >= 3','',false);
+    $this->db->where('pagos_generados >= 3', '',false);
     $contratos = $this->db->get('v_pagos_generados')->result_array();
     foreach ($contratos as $contrato) {
       if($contrato['pagos_generados'] >= 3){
-        suspender_contrato($contrato['contrato'],$contrato['id_cliente'], $this);
+        suspender_contrato($contrato['contrato'], $contrato['id_cliente'], $this);
       }
     }
   }
 
   private function backgroundPost($url){
     $parts=parse_url($url);
-
-    $fp = fsockopen($parts['host'],
-            isset($parts['port'])?$parts['port']:80,
+  
+    $fp = fsockopen($parts['host'], 
+            isset($parts['port'])?$parts['port']:80, 
             $errno, $errstr, 30);
-
+            
     if (!$fp) {
         return false;
     } else {
@@ -99,7 +100,7 @@ class Background extends CI_Controller{
         $out.= "Content-Type: application/x-www-form-urlencoded\r\n";
         $out.= "Connection: Close\r\n\r\n";
         if (isset($parts['query'])) $out.= $parts['query'];
-
+    
         fwrite($fp, $out);
         fclose($fp);
         return true;
@@ -110,10 +111,10 @@ class Background extends CI_Controller{
     $settings = $this->settings_model->get_settings();
     $hoy = date('Y-m-d');
     $generacion_factura = $settings['dia_generacion_factura'];
-
+    
     if (date('d') == $generacion_factura && $hoy != $settings['ultima_generacion_factura']) {
       $this->db->select('id_pago, id_contrato');
-      $this->db->where("(cast(fecha_limite as date) < cast(curdate() as date) or
+      $this->db->where("(cast(fecha_limite as date) < cast(curdate() as date) or 
                         (date_format(curdate(), '%d') = '$generacion_factura' and date_format(fecha_limite, '%m-%Y') = date_format(now(), '%m-%Y')))",'', false);
       $this->db->where('estado', 'no pagado');
       $this->db->where('generado', false);
