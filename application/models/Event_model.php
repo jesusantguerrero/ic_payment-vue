a4349da5ab55fc9cd60b51a12c5427706249076f
jesusantguerrero <me@jesusantguerrero.com>
2018-02-22 12:38:31 -0400

    :construction: add event concept

---
+++ application/models/Event_model.php
@@ -0,0 +1,24 @@
+<?php
+/**
+* IC Payment
+*@author Jesus Guerrero
+*@copyright Copyright (c) 2017 Insane Code
+*@version 1.0.0
+*
+*/
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Extra_model extends CI_MODEL{
+
+ public function __construct() {
+   parent::__construct();
+ }
+
+ public function add() {
+
+ }
+
+ public function get($event_id = null, $first_date = '2001-01-01', $second_date = null) {
+
+ }
+}
